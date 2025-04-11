44b48305c69afec9f433379e33d14150b0f2cffd
yassminemejri0 <yassminemejri9@gmail.com>
2025-04-11 10:54:08 +0100

    first commit

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads(){
+        System.out.println("it is a test class");
+        assertEquals(true, true);
+    }
+
+}
